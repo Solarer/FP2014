1b27e3c8e19da2a79f70a84bfd62a4e696ce6b68
Solarer <jan.adam@udo.edu>
2014-05-18 20:45:36 +0200

    Changes to be committed: 	new file:   04_Dipol/gnu/.relax12_gnu.swp 	modified:   04_Dipol/gnu/data 	renamed:    04_Dipol/gnu/relax2_gnu -> 04_Dipol/gnu/int1_gnu 	new file:   04_Dipol/gnu/int2_gnu 	modified:   04_Dipol/gnu/integral.py 	new file:   04_Dipol/gnu/output1 	new file:   04_Dipol/gnu/output2 	deleted:    04_Dipol/gnu/relax1.pdf 	modified:   04_Dipol/gnu/relax11.eps 	new file:   04_Dipol/gnu/relax11.pdf 	renamed:    04_Dipol/gnu/relax1_gnu -> 04_Dipol/gnu/relax11_gnu 	renamed:    04_Dipol/gnu/relax1.eps -> 04_Dipol/gnu/relax12.eps 	new file:   04_Dipol/gnu/relax12.pdf 	new file:   04_Dipol/gnu/relax12_gnu 	deleted:    04_Dipol/gnu/relax2.pdf 	new file:   04_Dipol/gnu/relax21.eps 	new file:   04_Dipol/gnu/relax21.pdf 	new file:   04_Dipol/gnu/relax21_gnu 	modified:   04_Dipol/gnu/relax22.eps 	new file:   04_Dipol/gnu/relax22.pdf 	new file:   04_Dipol/gnu/relax22_gnu 	new file:   04_Dipol/gnu/relax31.eps 	new file:   04_Dipol/gnu/relax31.pdf 	renamed:    04_Dipol/gnu/relax2.eps -> 04_Dipol/gnu/relax32.eps 	new file:   04_Dipol/gnu/relax32.pdf 	modified:   04_Dipol/tex/protokoll.pdf
    Auswertung 04 fertig, Werte fragwürdig

--- 04_Dipol/gnu/integral.py
+++ 04_Dipol/gnu/integral.py
@@ -1,21 +1,26 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def trapez(list):
-    size=55
+    size=len(list)
     x=np.zeros(size)
     y=np.zeros(size)
     output=np.zeros(size)
     for pos,element in enumerate(list):
         x[pos]=element[0]
         y[pos]=element[1]
-    print(x)
+    print(x[0])
     print(y)
 
     for i in range(size-1,0,-1):
         # print(i)
-        print(np.absolute((x[i]-x[i-1]))/2*(y[i]+y[i-1]))
+        # print(np.absolute((x[i]-x[i-1]))/2*(y[i]+y[i-1]))
         output[i-1]+=output[i]+np.absolute((x[i]-x[i-1]))/2*(y[i]+y[i-1])
-    print(output)
+
+    out_file=open('output','w')
+    for i,line in enumerate(output):
+        print(i)
+        out_file.write(str(x[i])+' '+str(output[i])+'\n')
 
 input1=[]
 input2=[]
@@ -28,8 +33,10 @@ for cnt,line in enumerate(file):
         if(cnt>42):
             input2.append((float(line[0]),float(line[1])))
 
-# print('Input1: ',input1)
+print('Input1: ',input1)
 # print('Input2: ',input2)
 
-trapez(input1)
+# trapez(input1)
 trapez(input2)
+
+print('el1: ',input1[0])
